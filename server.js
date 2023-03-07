7df04a0e98503f9db5a60b8d2acc9a6995e0f716
Kenji Fleming <kwf1999@gmail.com>
2023-03-06 21:14:33 -0500

    added functions and inquirer prompts to the server.js file for potential requests by the user

--- server.js
+++ server.js
@@ -1,9 +1,59 @@
 const express = require('express');
 const mysql = require('mysql2');
-// require("console.table");
+const cTable = require('console.table');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+function init() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'userChoice',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        }
+    ])
+    .then((answer) => {
+        switch (answer.userChoice) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                exit();
+                break;
+        }
+    });
+}
+
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -15,54 +65,175 @@ const db = mysql.createConnection({
 );
 
 //view all departments 
-app.get('/api/departments', (req, res) => {
-    const sql = `SELECT * FROM departments`;
+function viewDepartments() {
+    console.log('Viewing all departments');
 
-    db.query(sql, (err, rows) => {
+    db.query(`SELECT * FROM departments`, (err, result) => {
         if (err) {
-            res.status(500).json({ error: err.message });
-            return;
+            console.log(err);
         }
-        res.json({
-            message: 'success, here are the departments',
-            data: rows
-        });
+        console.tables(rows)
+        console.log('Success, here are all the departments: ' + result);
     });
-});
+};
 
 //view all roles 
-app.get('/api/roles', (req, res) => {
-    const sql = `SELECT * FROM roles`;
+function viewRoles() {
+    console.log('Viewing all roles');
 
-    db.query(sql, (err, rows) => {
+    db.query(`SELECT * FROM roles`, (err, result) => {
         if (err) {
-            res.status(500).json({ error: err.message });
-            return;
+            console.log(err);
         }
-        res.json({
-            message: 'success, here are all the roles',
-            data: rows
-        });
+        console.tables(result);
+        console.log('Success, here are all the roles: ' + result);
     });
-});
+};
 
 //view all employees
-app.get('/api/employees', (req, res) => {
-    const sql = `SELECT * FROM employees`;
+function viewEmployees() {
+    console.log('Viewing all employees');
 
-    db.query(sql, (err, rows) => {
+    db.query(`SELECT * FROM employees`, (err, result) => {
         if (err) {
-            res.status(500).json({ error: err.message });
-            return;
+            console.log(err);
         }
-        res.json({
-            message: 'success, here are all the employees',
-            data: rows
+        console.table(result);
+        console.log('Success, here are all the employees: ' + result);
+    });
+};
+
+//add a department
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: 'What is the name of the department you would like to add?'
+
+        }
+    ])
+    .then((answer) => {
+        const params = answer.department;
+        db.query(`INSERT INTO departments (name) VALUES (?)`, params, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log('Success, department added: ' + result);
+            console.table(result);
         });
     });
-});
+};
 
+//add a role
+function addRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the role you would like to add?'
+
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of the role you would like to add?'
+
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: 'What is the department id of the role you would like to add?'
+
+        }
+    ])
+    .then((answer) => {
+        const params = [answer.title, answer.salary, answer.department_id];
+        db.query(`INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`, params, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log('Success, role added: ' + result);
+            console.table(result);
+        });
+    });
+};
+
+//add an employee
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the first name of the employee you would like to add?'
+
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the last name of the employee you would like to add?'
+
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the role id of the employee you would like to add?'
+
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'What is the manager id of the employee you would like to add?'
+            
+        }
+    ])
+    .then((answer) => {
+        const params = [answer.first_name, answer.last_name, answer.role_id, answer.manager_id];
+        db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, params, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log('Success, employee added: ' + result);
+            console.table(result);
+        });
+    });
+};
+
+//update an employee role
+function updateEmployeeRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the role id of the employee you would like to update?'
+
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the id of the employee you would like to update?'
+
+        }
+    ])
+    .then((answer) => {
+        const params = [answer.role_id, answer.id];
+        db.query(`UPDATE employees SET role_id = ? WHERE id = ?`, params, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log('Success, employee role updated: ' + result);
+            console.table(result);
+        });
+    });
+};
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+init();
